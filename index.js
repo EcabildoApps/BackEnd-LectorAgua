66486325573b56efb72d6db0a13376b325529888
Ecabildo <appecabildo@gmail.com>
2025-03-05 11:57:46 -0500

    https

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 const db = require('./models');
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const path = require('path');
 require('dotenv').config();
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'server.key')),  // Ruta al archivo de la clave privada
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'server.cert'))  // Ruta al archivo del certificado
+};
 
 
 app.use(cors({
@@ -29,9 +36,8 @@ app.use((req, res, next) => {
 
 
 // Iniciar servidor
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en puerto: ${port} en modo ${process.env.NODE_ENV}`);
+https.createServer(options, app).listen(process.env.PORT || 3000, () => {
+    console.log(`Servidor HTTPS corriendo en puerto: ${process.env.PORT || 3000} en modo ${process.env.NODE_ENV}`);
 });
 
 // Conectar con la base de datos
